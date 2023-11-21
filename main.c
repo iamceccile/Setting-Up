9abf070e8c93b790a928e21ccaeb2c2433fa1772
iamceccile <115502810+iamceccile@users.noreply.github.com>
2023-11-21 18:51:58 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "my.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 int main(int argc, char *argv[ ])
 {
+    if (argv[1] == NULL || open(argv[1], O_RDONLY) == -1) {
+    mini_printf("please add arg\n");
+    } else {
     fs_open_file(argv[1]);
     fs_cat_500_bytes(argv[1]);
     fs_print_first_line(argv[1]);
     char arr [] = "hello, world";
     array_1d_print_chars(arr);
+     }
 }
